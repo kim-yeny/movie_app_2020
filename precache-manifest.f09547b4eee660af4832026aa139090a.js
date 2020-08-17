ad95adc8edab678ab654e1145fe1beeda686bbe7
kim_yeny <des423@naver.com>
2020-08-17 21:45:04 +0900

    Updates

---
+++ precache-manifest.f09547b4eee660af4832026aa139090a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331e9e94b7f1eb547edd643daf0dd2d0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "932e35362e8c66030ea5",
+    "url": "/movie_app_2020/static/css/main.58aec287.chunk.css"
+  },
+  {
+    "revision": "bce2e33464bf27556e07",
+    "url": "/movie_app_2020/static/js/2.d3e6f99b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.d3e6f99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932e35362e8c66030ea5",
+    "url": "/movie_app_2020/static/js/main.b2dfdbe1.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
